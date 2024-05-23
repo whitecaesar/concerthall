86b18c247b98d1fba4fee58c7c80d142baf3ce60
YS JEONG <whitecaesar@anytoon.kr>
2024-05-23 11:00:40 +0900

    즐겨찾기

--- src/component/organism/likeList/LikeList.tsx
+++ src/component/organism/likeList/LikeList.tsx
@@ -1,11 +1,17 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import style from "./likeList.module.css";
 import SubTitleProvider from "@/providers/SubTitleProvider";
 import { getAlbumAxios } from "@/services/contents/AlbumAxios";
 import { useQuery } from "@tanstack/react-query";
 import AlbumViewAll from "@/component/template/AlbumViewAll";
 import TrackList from "@/component/organism/trackList/TrackList";
+import { TRACK_RECENT_LIST_RESPONSE } from "@/services/contents/RecentTrackListAxios";
+import { getLikeTrackListAxios } from "@/services/contents/LikeTrackListAxios";
+import RecentTrackList from "../singleList/RecentTrackList";
+import RCTTrackList from "../trackList/RCTTrackList";
+import { ALBUM_LIKE_LIST_RESPONSE, getLikeAlbumListAxios } from "@/services/contents/LikeAlbumListAxios";
+import LikeAlbumList from "../albumList/LikeAlbumList";
 
 type Props = {};
 interface AlbumTrackProps {
@@ -13,23 +19,20 @@ interface AlbumTrackProps {
 }
 export default function LikeList(album: AlbumTrackProps, props: Props) {
 	const [activeTab, setActiveTab] = useState<string>("Tab1");
+	const [trackList, setTrackList] = useState<TRACK_RECENT_LIST_RESPONSE>();
+	const [AlbumList, setAlubmList] = useState<ALBUM_LIKE_LIST_RESPONSE>();
+	//const [ArtistList, setArtistList] = useState<TRACK_RECENT_LIST_RESPONSE>();
+	//const [PlayList, setRecentTrackList] = useState<TRACK_RECENT_LIST_RESPONSE>();
+
+	useEffect(() => {
+		// const recent = ;
+		getLikeTrackListAxios().then((data) => setTrackList(data));
+		getLikeAlbumListAxios().then((data) => setAlubmList(data));
+	}, []);
 
 	const handleTabClick = (tabName: string) => {
 		setActiveTab(tabName);
 	};
-
-	const { data, isError, isLoading } = useQuery({
-		queryKey: ["ALBUM-ITEM"],
-		queryFn: () => {
-			const TrackList = getAlbumAxios(album.album_id);
-			return TrackList;
-		},
-	});
-
-	if (isLoading) return <div>Loading...</div>;
-	if (isError || !data) return <div>Error occurred</div>;
-
-	const AlbumItem = data?.ITME_INFO;
 
 	return (
 		<>
@@ -39,13 +42,13 @@ export default function LikeList(album: AlbumTrackProps, props: Props) {
 						className={activeTab === "Tab1" ? style.active : ""}
 						onClick={() => handleTabClick("Tab1")}
 					>
-						Tracks(25)
+						Tracks({trackList?.totalCount})
 					</button>
 					<button
 						className={activeTab === "Tab2" ? style.active : ""}
 						onClick={() => handleTabClick("Tab2")}
 					>
-						Albums(2)
+						Albums({AlbumList?.totalCount})
 					</button>
 					<button
 						className={activeTab === "Tab3" ? style.active : ""}
@@ -61,25 +64,26 @@ export default function LikeList(album: AlbumTrackProps, props: Props) {
 					</button>
 				</div>
 
+
 				<div className={style.tabContent}>
 					{activeTab === "Tab1" && (
 						<div>
-							{/*<TrackList trackList={AlbumItem} />*/}
+							{trackList && <RCTTrackList trackList={trackList}/>}
 						</div>
 					)}
 					{activeTab === "Tab2" && (
 						<div>
-							{/*<AlbumViewAll />*/}
+							{AlbumList && <LikeAlbumList likeAlbumList={AlbumList}/>}
 						</div>
 					)}
 					{activeTab === "Tab3" && (
 						<div>
-							{/*<TrackList trackList={AlbumItem} />*/}
+
 						</div>
 					)}
 					{activeTab === "Tab4" && (
 						<div>
-							{/*<AlbumViewAll />*/}
+							
 						</div>
 					)}
 				</div>
--- src/services/contents/LikeAlbumListAxios.ts
+++ src/services/contents/LikeAlbumListAxios.ts
@@ -1,26 +1,10 @@
 import axios, { AxiosResponse } from "axios";
-import {TRACK_TRACKS_ITEM_TYPE } from "./PlayListTrackAxios";
+import { TRACK_TRACKS_ITEM_TYPE } from "./PlayListTrackAxios";
+import { ALBUM_RECENT_ITEM_TYPE } from "./RecentAlbumAxios";
 
 const token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIzMDI5IiwiaWF0IjoxNzExNTAzMDA0LCJleHAiOjIwMjY4NjMwMDQsImlkIjozMDI5LCJuYW1lIjoiSmFtaWUiLCJubyI6MzAyOSwiZnJpZW5kIjoyMzY1LCJyb3NldHViZVJlY2VudFBsYXlsaXN0IjoxNTUwNCwidXNlcm5hbWUiOiJKYW1pZSIsImlzRW1haWxDb25maXJtIjpmYWxzZSwiYXV0aG9yaXRpZXMiOlsiUk9TRV9NRU1CRVIiLCJST0xFX1VTRVJfT05MSU5FIl19.3ZLPsp98wTCgMOChwwm2XtzRhKO7bMih556OtA6tnzvWAM_xSUSFtdMrlXCZR0k5142qpG3Cxd1L33qkRkPAaw"; // 동적으로 토큰을 얻는 로직
 
-export type ALBUM_RECENT_ITEM_TYPE = {
-	id: string;
-	title: string;
-	clientKey: string;
-	tracks?: TRACK_TRACKS_ITEM_TYPE[];
-	favorite: boolean;
-    star: number;
-    thumbnail:string;
-    type: string;
-    playTime?: string;
-};
-
-export type ALBUM_RECENT_LIST_TYPE = {
-	type: string;
-	album: ALBUM_RECENT_ITEM_TYPE;
-};
-
-export type ALBUM_RECENT_LIST_RESPONSE = {
+export type ALBUM_LIKE_LIST_RESPONSE = {
 	message: string;
 	code: string;
 	size: number;
@@ -28,16 +12,14 @@ export type ALBUM_RECENT_LIST_RESPONSE = {
     totalCount: number;
     last: boolean;
     offset:number;
-    recentList: ALBUM_RECENT_LIST_TYPE[];
+    albums: ALBUM_RECENT_ITEM_TYPE[];
 
 };
 
-export async function getRecentAlbumAxios(
-	mediaTypes?: string, // idAlbum 파라미터를 추가했습니다.
-	size?:number
-): Promise<ALBUM_RECENT_LIST_RESPONSE> {
-	const response: AxiosResponse<ALBUM_RECENT_LIST_RESPONSE> = await axios.get(
-		`https://dev.api.roseaudio.kr/v1/member/album/recent?mediaTypes=CONCERT_HALL&page=0&size=${size}`,{
+export async function getLikeAlbumListAxios(
+): Promise<ALBUM_LIKE_LIST_RESPONSE> {
+	const response: AxiosResponse<ALBUM_LIKE_LIST_RESPONSE> = await axios.get(
+		`https://dev.api.roseaudio.kr/v1/member/album/favorite?mediaType=QOBUZ&sortType=TITLE_ASC`,{
 		headers: {
 			'Authorization': `Bearer ${token}`
 		} // URL 구성을 동적으로 변경했습니다.
@@ -49,3 +31,5 @@ export async function getRecentAlbumAxios(
 		throw new Error(`에러입니다. ${response.status}`);
 	}
 }
+
+
