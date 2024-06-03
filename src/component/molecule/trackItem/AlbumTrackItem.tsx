30224e2e87f5d95590c5fcca0274c41f6ad6003e
eungom <eungom@anytoon.kr>
2024-06-03 09:09:00 +0900

    앨범 트랙 > 아티스트 레이아웃 수정

--- src/component/molecule/trackItem/AlbumTrackItem.tsx
+++ src/component/molecule/trackItem/AlbumTrackItem.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import LikeButton from "@/component/atom/button/LikeButton";
 import FuncButton from "@/component/atom/button/FuncButton";
 import style from "./trackItem.module.css";
-import { funcAlbumTrackPlayClick} from "@/services/common";
+import { funcAlbumTrackPlayClick } from "@/services/common";
 import { useQuery } from "@tanstack/react-query";
 import { getPlayInfoAxios } from "@/services/contents/PlayInfoAxios";
 import { getTrackAxios } from "@/services/contents/TrackAxios";
@@ -17,16 +17,27 @@ interface TrackItemProps {
 	AlbumTrackList: ALBUM_ITEM_TYPE[];
 }
 
-export default function AlbumTrackItem({ albumTrackInfo, AlbumTrackList }: TrackItemProps) {
-	const { data : trackData, isError, isLoading } = useQuery({
+export default function AlbumTrackItem({
+	albumTrackInfo,
+	AlbumTrackList,
+}: TrackItemProps) {
+	const {
+		data: trackData,
+		isError,
+		isLoading,
+	} = useQuery({
 		queryKey: ["TRACK-LIST"],
 		queryFn: () => {
 			const TrackItem = getTrackAxios(albumTrackInfo.ID);
 			return TrackItem;
 		},
 	});
-	
-	const { data :playData, isError: playError, isLoading: playLoding } = useQuery({
+
+	const {
+		data: playData,
+		isError: playError,
+		isLoading: playLoding,
+	} = useQuery({
 		queryKey: ["PLAY-INFO"],
 		queryFn: () => {
 			const PlayInfo = getPlayInfoAxios(albumTrackInfo.ID);
@@ -34,31 +45,46 @@ export default function AlbumTrackItem({ albumTrackInfo, AlbumTrackList }: Track
 		},
 	});
 
-	if (isLoading || playLoding ) return <div>Loading...</div>;
-	if (isError || playError || !playData || !trackData) return <div>Error occurred</div>;
+	if (isLoading || playLoding) return <div>Loading...</div>;
+	if (isError || playError || !playData || !trackData)
+		return <div>Error occurred</div>;
 
 	return (
 		<div className={style.trackItem}>
-			<span onClick={() => funcAlbumTrackPlayClick('trackPlay',playData, albumTrackInfo)}>
-				<span>
-				{/* Link에는 트랙 재생하는 url이 들어가야 함 */}
-				<Image
-					src={albumTrackInfo.THUMBNAIL}
-					alt={albumTrackInfo.TITLE}
-					width={45}
-					height={45}
-					priority={true}
-					className={style.thumbnail}
-				/>
-				<p className={style.title}>{albumTrackInfo.TITLE}</p>
+			<span className={style.albumTrackInfoWrap}>
+				<span
+					className={style.albumTrackInfo}
+					onClick={() =>
+						funcAlbumTrackPlayClick("trackPlay", playData, albumTrackInfo)
+					}
+				>
+					{/* Link에는 트랙 재생하는 url이 들어가야 함 */}
+					<Image
+						src={albumTrackInfo.THUMBNAIL}
+						alt={albumTrackInfo.TITLE}
+						width={45}
+						height={45}
+						priority={true}
+						className={style.thumbnail}
+					/>
+					<p className={style.title}>{albumTrackInfo.TITLE}</p>
 				</span>
-				{albumTrackInfo.ARTIST?.map((item, index) => (
-				<Link key={index} href={`/artist/${item.artist_id}`}><p className={style.artist}>{item.artist_name}</p></Link>
-				))}				
+				<div className={style.albumArtistInfo}>
+					{albumTrackInfo.ARTIST?.map((item, index) => (
+						<Link key={index} href={`/artist/${item.artist_id}`}>
+							<p className={style.artist}>{item.artist_name}</p>
+						</Link>
+					))}
+				</div>
 			</span>
 			<div className={style.buttonGroup}>
-				<AlbumTrackLikeButton track_info={albumTrackInfo}/>
-				<AlbumFuncButton method="albumTrackMore" track_info={trackData} play_info={playData} albumTrackList={AlbumTrackList}/>
+				<AlbumTrackLikeButton track_info={albumTrackInfo} />
+				<AlbumFuncButton
+					method="albumTrackMore"
+					track_info={trackData}
+					play_info={playData}
+					albumTrackList={AlbumTrackList}
+				/>
 			</div>
 		</div>
 	);
